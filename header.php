f29bc66633963e4b0a3c8d5012509384db2c7468
Muneth <muneetchaudhary@me.com>
2021-02-23 13:53:53 +0100

    home link fix

--- header.php
+++ header.php
@@ -29,7 +29,7 @@
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#cases">Cases</a></li>
-          <li><a href="#blog">Blog</a></li>
+          <li><a href="blog.php">Blog</a></li>
 
           <?php
 
--- headerblog.php
+++ headerblog.php
@@ -19,10 +19,10 @@
       </h1>
       <nav>
         <ul>
-        <li><a href="#home">Home</a></li>
+        <li><a href="index.php">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#cases">Cases</a></li>
-          <li><a href="#blog">Blog</a></li>
+          <li><a href="blog.php">Blog</a></li>
           <li><a href="signup.php">Sign up</a></li>
           <li><a href="login.php">Log in</a></li>
         </ul>
