866f390995c2dc6823ea7b6e25a1eeff15005d40
Muneth <muneetchaudhary@me.com>
2021-02-23 13:31:40 +0100

    db

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,14 +1,15 @@
 <?php   
 
-$serverName = "mysql-21123-0.cloudclusters.net";
+$serverName = "mysql-21117-0.cloudclusters.net";
 $dbUsername = "adminmuneth";
 $dbPassword = "password";
 $dbName = "phpproject01";
+$port = " 21123";
 
 // using mysqli procedural way to connect to the database...
 // not using normal mySQL way 
 
-$conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName);
+$conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName, $port);
 
 if(!$conn) {
     die("Connection failed: " . mysqli_connect_error());
