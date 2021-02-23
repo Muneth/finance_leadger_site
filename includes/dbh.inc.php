5092b54a4ba37d5501c219c130f25cb7c151bf3c
Muneth <muneetchaudhary@me.com>
2021-02-23 13:32:46 +0100

    db

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -4,7 +4,7 @@ $serverName = "mysql-21117-0.cloudclusters.net";
 $dbUsername = "adminmuneth";
 $dbPassword = "password";
 $dbName = "phpproject01";
-$port = " 21123";
+$port = "21123";
 
 // using mysqli procedural way to connect to the database...
 // not using normal mySQL way 
